77f60ec8f2c329307b8dc36bcbdd6959353a74ad
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2024-09-27 22:43:21 +0530

    Create 03.Reverse_String1.cpp

---
+++ Problems/Problem1/03.Reverse_String1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string reverseString(const string& str) 
+{
+    string reversed;
+    for (int i = str.length() - 1; i >= 0; i--) 
+    {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input); // Use getline to include spaces
+    string reversed = reverseString(input);
+    cout << "Reversed string: " << reversed << endl;
+
+    return 0;
+}
