434a564818c8c4270de44a4d1c5d5017c61150c1
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2024-12-02 13:50:31 +0530

    Update Create.cpp

--- LinkedList/Create.cpp
+++ LinkedList/Create.cpp
@@ -1,59 +1,56 @@
 #include <iostream>
 using namespace std;
 
-struct Node 
-{
+struct Node {
     int data;   // Data to store
     Node* next; // Pointer to the next node
-
+    // Constructor to initialize the node with a value
     Node(int val) : data(val), next(nullptr) {}
 };
 
 class LinkedList 
 {
     private:
-    Node* head; 
+    Node* head; // Points to the first node in the list
     public:
     LinkedList() : head(nullptr) {}
     void append(int value) 
     {
-        Node* newNode = new Node(value);  // Create a new node
-        if (!head) 
-        {
-            head = newNode; // If the list is empty, the new node becomes the head
+        Node* newNode = new Node(value);  
+        if (head == nullptr) 
+        {             
+            head = newNode;                
             return;
         }
-        // Otherwise, find the last node and link the new node
         Node* temp = head;
-        while (temp->next) 
+        while (temp->next != nullptr) 
         {
             temp = temp->next;
         }
         temp->next = newNode;
     }
-    // Function to insert a node at the beginning of the list
     void insertAtBeginning(int value) 
     {
-        Node* newNode = new Node(value);  // Create a new node
-        newNode->next = head;  // New node points to the current head
-        head = newNode;  // New node becomes the new head
+        Node* newNode = new Node(value);  
+        newNode->next = head;  
+        head = newNode;  
     }
-    // Function to print the entire list
     void printList() const 
     {
         Node* temp = head;
-        while (temp) 
-        {
+        while (temp != nullptr) 
+        {  
             cout << temp->data << " ";
             temp = temp->next;
         }
         cout << endl;
     }
+
     ~LinkedList() 
     {
         Node* temp;
-        while (head) 
-        {
+        while (head != nullptr) 
+        {  
             temp = head;
             head = head->next;
             delete temp;
@@ -61,23 +58,22 @@ class LinkedList
     }
 };
 
-int main() 
-{
+// Main function to test the linked list
+int main() {
     LinkedList list;
+
+    // Add elements to the linked list
     list.append(10);
     list.append(20);
     list.append(30);
     list.append(40);
-    // Print the list
+
     cout << "Linked List: ";
     list.printList();  // Output: 10 20 30 40
 
-    // Insert a node at the beginning
     list.insertAtBeginning(5);
     cout << "After inserting 5 at the beginning: ";
     list.printList();  // Output: 5 10 20 30 40
 
     return 0;
 }
-
-
