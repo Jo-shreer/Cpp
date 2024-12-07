3fa910ffda9500fbd1d0df4bc638be08686b03d1
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2024-12-07 22:04:50 +0530

    Update 01.Vector.cpp

--- STL/01.Vector.cpp
+++ STL/01.Vector.cpp
@@ -1,39 +1,30 @@
 #include <iostream>
 #include <vector>
-
-int main() {
-    // Create a vector of integers
+int main() 
+{
     std::vector<int> numbers;
-
-    // Adding elements to the vector
     numbers.push_back(10);
     numbers.push_back(20);
     numbers.push_back(30);
-
-    // Displaying elements in the vector
     std::cout << "Vector elements: ";
-    for (const int& num : numbers) {
-        std::cout << num << " "; // Output: 10 20 30
+    for (const int& num : numbers) 
+    {
+        std::cout << num << " ";
     }
     std::cout << std::endl;
-
-    // Accessing and modifying elements
-    numbers[1] = 25; // Change the second element (20 to 25)
-
+    numbers[1] = 25;
     std::cout << "After modification: ";
-    for (const int& num : numbers) {
-        std::cout << num << " "; // Output: 10 25 30
+    for (const int& num : numbers) 
+    {
+        std::cout << num << " "; 
     }
     std::cout << std::endl;
-
-    // Removing the last element
-    numbers.pop_back(); // Remove the last element (30)
-
+    numbers.pop_back();
     std::cout << "After pop: ";
-    for (const int& num : numbers) {
-        std::cout << num << " "; // Output: 10 25
+    for (const int& num : numbers) 
+    {
+        std::cout << num << " "; 
     }
     std::cout << std::endl;
-
     return 0;
 }
