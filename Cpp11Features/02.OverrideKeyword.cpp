dd11c0adbe1e39ac0b4666a80759914bfac90c9d
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2024-12-08 20:59:28 +0530

    Update 02.OverrideKeyword.cpp

--- Cpp11Features/02.OverrideKeyword.cpp
+++ Cpp11Features/02.OverrideKeyword.cpp
@@ -1,15 +1,26 @@
+/*
+The override keyword in C++ is used to explicitly indicate that a function is intended to override a virtual function in a base class.
+Purpose of the override Keyword:
+Clarification: It makes the programmer's intent clear, signaling that a function is meant to override a base class virtual function.
+Error Prevention: If the function does not actually override a virtual function (due to a mismatch in the function signature or because the base class 
+function is not virtual), the compiler will generate an error. This prevents subtle bugs that might arise from incorrectly overriding a function.
+*/
 #include <iostream>
 
-class Base {
-public:
-    virtual void print() {
+class Base 
+{
+    public:
+    virtual void print() 
+    {
         std::cout << "Base class print" << std::endl;
     }
 };
 
-class Derived : public Base {
+class Derived : public Base 
+{
 public:
-    void print() override { // Correctly overrides Base::print
+    void print() override // Correctly overrides Base::print
+    {
         std::cout << "Derived class print" << std::endl;
     }
     // Uncommenting the following will cause a compile-time error
