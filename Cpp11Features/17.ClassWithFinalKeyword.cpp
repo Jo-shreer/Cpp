471ddb6367b75effcd6183cccc8490c243094625
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2024-12-08 21:04:55 +0530

    Update 17.ClassWithFinalKeyword.cpp

--- Cpp11Features/17.ClassWithFinalKeyword.cpp
+++ Cpp11Features/17.ClassWithFinalKeyword.cpp
@@ -1,16 +1,29 @@
+/*
+In C++, the final keyword is used to indicate that a function or class cannot be further overridden or inherited, respectively.
+It was introduced in C++11 to enhance control over class inheritance and polymorphism. The final keyword provides a way to prevent unintended 
+overriding or further inheritance, which helps improve code safety and clarity.
+Use of final in C++
+Final for Virtual Functions: The final keyword can be applied to virtual functions to indicate that they cannot be overridden by any derived class.
+Final for Classes: The final keyword can be applied to a class to prevent it from being inherited by any other class.
+
+*/
+
 #include <iostream>
 
-class Base {
-public:
+class Base 
+{
+    public:
     void show() {
         std::cout << "Base class" << std::endl;
     }
 };
 
 // Final class
-class FinalClass final {
-public:
-    void display() {
+class FinalClass final 
+{
+    public:
+    void display() 
+    {
         std::cout << "Final class" << std::endl;
     }
 };
@@ -18,7 +31,8 @@ public:
 // Attempting to derive from FinalClass will cause a compile error
 // class Derived : public FinalClass { }; // Uncommenting this will give an error
 
-int main() {
+int main() 
+{
     FinalClass fc;
     fc.display();
     return 0;
