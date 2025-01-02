9f2410af16cb4b3268f87e4d0212bb23d3e3b20f
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-01-02 12:39:19 +0530

    Update 04.findAllSubarraysWithGivenSum.cpp

--- Problems/Problem1/04.findAllSubarraysWithGivenSum.cpp
+++ Problems/Problem1/04.findAllSubarraysWithGivenSum.cpp
@@ -8,25 +8,24 @@ int findAllSubarraysWithGivenSum(vector < int > & arr, int k)
     int n = arr.size(); // size of the given array.{3, 1, 2, 4}, K = 6
     map<int, int> mpp;
     int preSum = 0;
-	int cnt = 0;
+    int cnt = 0;
 	
     mpp[0] = 1; 
     
-    for (int i = 0; i < n; i++) 
+        for (int i = 0; i < n; i++) 
 	{
         preSum += arr[i];
         cout <<"preSum:" << preSum << endl;
 
         int remove = preSum - k;			// Calculate x-k:
         cout << "remove:" << remove << endl;
-
-        
+		
         cnt += mpp[remove];					// Add the number of subarrays to be removed:
         cout <<"cnt:" << cnt << endl;
         mpp[preSum] += 1;
 		
         for (auto value: mpp)
-		{
+	{
             cout <<"value.first:" << value.first << endl;
             cout <<"value.second:" << value.second << endl;
         }
