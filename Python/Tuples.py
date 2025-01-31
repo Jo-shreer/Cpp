3c209297dbaba0007f086a12cc4da8681283f9b4
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-01-31 18:17:19 +0530

    Create Tuples.py

---
+++ Python/Tuples.py
@@ -0,0 +1,9 @@
+#Tuples are similar to lists but are immutable (cannot be changed after creation). 
+They are defined using parentheses ().
+
+# Tuple example
+coordinates = (10, 20)
+print(coordinates[0])  # Accessing elements
+
+# Tuples are immutable, so you can't do this:
+# coordinates[0] = 30  # This would throw an error
