6f0395108ce4af71cf6a85d2879993842a9a278b
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-01-31 18:24:34 +0530

    Create Sets.py

---
+++ Python/Sets.py
@@ -0,0 +1,7 @@
+#Sets are unordered collections of unique elements. They are defined using curly braces {}.
+
+# Set example
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")  # Adding an element
+fruits.remove("banana")  # Removing an element
+print(fruits)
