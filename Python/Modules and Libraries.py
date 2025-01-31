be80b6adf9e4efb6b134b49355e99ae35abb2a58
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-01-31 18:29:14 +0530

    Create Modules and Libraries.py

---
+++ Python/Modules and Libraries.py
@@ -0,0 +1,12 @@
+#You can import and use external Python libraries and modules. 
+#Python has a huge collection of built-in modules, and you can also install third-party libraries via pip.
+
+# Importing a built-in module
+import math
+
+print(math.sqrt(16))  # Square root of 16
+
+
+#To install third-party libraries, you use:
+pip install requests  # Example for installing the requests library
+
