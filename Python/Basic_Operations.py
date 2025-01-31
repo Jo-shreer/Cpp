fa6a7039bb4da1490b27e7a858fdad70b3851853
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-01-31 18:14:52 +0530

    Create Basic_Operations.py

---
+++ Python/Basic_Operations.py
@@ -0,0 +1,32 @@
+#Python supports basic mathematical operations like addition, subtraction, multiplication, etc.
+# Operations
+a = 10
+b = 3
+
+print(a + b)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Multiplication
+print(a / b)  # Division
+
+#Conditional Statements (if-else)
+age = 20
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+#  Loops are used to repeatedly execute code. Python has two main types: for and while.
+# Iterating through a list
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+
+#while
+# Loop until condition is False
+counter = 0
+while counter < 5:
+    print(counter)
+    counter += 1  # Increment counter
+
