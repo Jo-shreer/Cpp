20ae1482b859b1d6ebb176e2d12d2f2dd28f9b81
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-01-31 18:11:07 +0530

    Create BasicVariables.py

---
+++ Python/BasicVariables.py
@@ -0,0 +1,8 @@
+#In Python, variables are used to store data. Data types define what kind of data is stored in a variable.
+
+# Variables and their types
+
+x = 5             # Integer
+y = 3.14          # Float
+name = "John"     # String
+is_active = True  # Boolean
