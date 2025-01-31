0972aaba648c2c8e7c1ff85e1450ba68de5ecae0
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-01-31 18:26:51 +0530

    Create File_Handling.py

---
+++ Python/File_Handling.py
@@ -0,0 +1,6 @@
+#Python can be used to read and write files. This is useful for working with data files (e.g., text files, CSVs).
+
+# Writing to a file
+
+with open("example.txt", "w") as file:
+    file.write("Hello, Python!")
