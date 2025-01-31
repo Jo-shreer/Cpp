d15a3d700024d219082d36b67c3f6aa6dbb500a9
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-01-31 18:25:11 +0530

    Create List Comprehension.py

---
+++ Python/List Comprehension.py
@@ -0,0 +1,5 @@
+#List comprehension provides a concise way to create lists. It's a more readable and efficient alternative to traditional loops.
+
+# Example of list comprehension
+squares = [x**2 for x in range(10)]  # List of squares from 0 to 9
+print(squares)
