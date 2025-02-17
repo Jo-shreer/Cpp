0f19535329b046488b1d4b60cece3aebf2155b6f
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-02-17 17:35:39 +0530

    Update 11.Runtime_Polymorphism.cpp

--- BasicConcepts/11.Runtime_Polymorphism.cpp
+++ BasicConcepts/11.Runtime_Polymorphism.cpp
@@ -28,3 +28,39 @@ int main() {
     delete animals[0];
     return 0;
 }
+
+
+
+OR
+
+*********************************************************************************************************************
+class Base
+{
+	public:
+	virtual void show()
+	{
+		cout << "In Base" << endl;
+	}
+};
+
+class Derived : public Base
+{
+	public:
+	void show()
+	{
+		cout << "In Derived"  << endl;
+	}
+};
+
+int main()
+{
+	Base *bp = new Derived();
+	unique_ptr<Base> bp1 = make_unique<Derived>();
+	bp1->show();
+	bp->show();
+	return 0;
+}
+
+
+
+
