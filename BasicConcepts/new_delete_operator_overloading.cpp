a430a467001903f7c4f5f3db052f2f960bf6e8c8
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-02-17 17:45:24 +0530

    Update new_delete_operator_overloading.cpp

--- BasicConcepts/new_delete_operator_overloading.cpp
+++ BasicConcepts/new_delete_operator_overloading.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
-#include <cstdlib>
+#include <new>  // Required for placement new
 
-// Overriding the global new operator
+// Overriding global new operator (without malloc)
 void* operator new(std::size_t size) 
 {
     std::cout << "Custom new called, size: " << size << std::endl;
-    void* ptr = std::malloc(size);  
+    void* ptr = ::operator new(size);  
     if (!ptr) 
     {
-        throw std::bad_alloc(); 
+        throw std::bad_alloc();  // Throw exception if allocation fails
     }
     return ptr;
 }
 
-// Overriding the global delete operator
+// Overriding global delete operator (without free)
 void operator delete(void* ptr) noexcept 
 {
     std::cout << "Custom delete called" << std::endl;
-    std::free(ptr);  
+    ::operator delete(ptr);  // Use C++ operator delete
+}
 
 class MyClass 
 {
@@ -26,10 +27,11 @@ class MyClass
     MyClass(int val) : x(val) {}
 };
 
-int main() 
-{
+int main() {
+    // Using the new operator (which will call the custom new)
     MyClass* obj = new MyClass(10);
     std::cout << "MyClass value: " << obj->x << std::endl;
+    // Using the delete operator (which will call the custom delete)
     delete obj;
     return 0;
 }
