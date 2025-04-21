a4360a4a1dc766ed547a2525b854356e92ba1cac
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-04-21 20:32:11 +0100

    Create ConditionVariableInMultithreading.cpp

---
+++ BasicConcepts/ConditionVariableInMultithreading.cpp
@@ -0,0 +1,3 @@
+/*A condition variable is a synchronization primitive that allows threads to wait (block) until notified by another thread 
+that some condition is true. It helps when one thread depends on another to complete a task or update shared data.
+*/
