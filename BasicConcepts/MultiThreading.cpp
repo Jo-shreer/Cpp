666b8fbe9484dbafd1a8c83b7579868be183c4b6
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-04-21 19:58:30 +0100

    Update MultiThreading.cpp

--- BasicConcepts/MultiThreading.cpp
+++ BasicConcepts/MultiThreading.cpp
@@ -1,5 +1,5 @@
 
-/*************************************Basic***********************/
+/*************************************Creation of thread***********************/
 #include <iostream>
 #include <thread>
 
@@ -21,3 +21,61 @@ int main()
     cout << "Main thread ends." << endl;
     return 0;
 }
+
+/************************************Passing an argument***********************/
+
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void printNumber(int x) 
+{
+    cout << "Number: " << x << endl;
+}
+
+int main() {
+    thread t(printNumber, 42);
+    t.join();
+}
+
+
+/***********************************Using Lambda with Threads***********************/
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+int main() {
+    thread t([]() {
+        cout << "Hello from lambda thread!" << endl;
+    });
+    t.join();
+}
+
+/***********************************DetachThreads***********************/
+/*What does detach() do?
+It tells the system that the thread's resources should be automatically reclaimed once it finishes execution.
+
+After calling detach(), you no longer have control over the thread — you can't join() it.
+
+It allows the thread to execute in the background.*/
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+void backgroundTask() 
+{
+    cout << "Running in background..." << endl;
+}
+
+int main() {
+    thread t(backgroundTask);
+    t.detach(); // Let it run independently
+    this_thread::sleep_for(chrono::seconds(1));
+}
+
+
+
