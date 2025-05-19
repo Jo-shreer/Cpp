bb1693946d4a74dc189b15f5bb6a79fdc8fc73ed
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-05-19 21:58:42 +0100

    Create PointerBasics.c

---
+++ C/PointerBasics.c
@@ -0,0 +1,7 @@
+
+
+const int *ptr -   Pointer to a constant int – can't change the value pointed to.
+  
+int *const ptr  -  Constant pointer to an int – can't change the pointer.
+  
+const int *const ptr  - Constant pointer to a constant int – can't change either.
