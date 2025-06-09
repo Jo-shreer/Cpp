7723024d5cf419bbd75784be196514925dc55d20
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-06-09 08:56:44 +0100

    Update 01. constexpr.cpp

--- Cpp11Features/01. constexpr.cpp
+++ Cpp11Features/01. constexpr.cpp
@@ -1,16 +1,23 @@
-/*A function that is marked constexpr is evaluated at compile time if its arguments are constant expressions. 
-A constexpr function must have a body, and its return value must be determinable at compile time.
+/*A function that is marked constexpr is evaluated at compile time 
+if its arguments are constant expressions. 
+A constexpr function must have a body, and its return value 
+must be determinable at compile time.
 
 *Benefits
 1. Compile-Time Computation
-The most significant advantage of constexpr is that it allows computations to be performed at compile time, rather than at runtime. 
-This can result in major performance improvements, especially for computations that would otherwise have to be repeated every time the program runs.
+The most significant advantage of constexpr is that it allows computations
+to be performed at compile time, rather than at runtime. 
+This can result in major performance improvements,especially for 
+computations that would otherwise have to be repeated every time the
+program runs.
 2. Faster Execution
-By using constexpr, you can avoid redundant runtime calculations, leading to faster execution. 
+By using constexpr, you can avoid redundant runtime calculations, 
+leading to faster execution. 
 If a value is computed during program startup (or even before), you don’t need to recompute it each time it is used.
 
 *Limitations of constexpr Functions:
-Complexity: A constexpr function cannot contain code that cannot be evaluated at compile time, such as:
+Complexity: A constexpr function cannot contain code that cannot be 
+evaluated at compile time, such as:
 Loops with non-constant bounds
 Dynamic memory allocation
 Use of non-constant variables
