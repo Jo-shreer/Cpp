f0c9eec162ca977b6cc00d77cb4bfd5bedd9cf43
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-07-18 10:27:45 +0100

    Update Find Employees Who Belong to the Same Department as 'John Doe'.sql

--- SQL/Find Employees Who Belong to the Same Department as 'John Doe'.sql
+++ SQL/Find Employees Who Belong to the Same Department as 'John Doe'.sql
@@ -1,3 +1,6 @@
+Find Employees Who Belong to the Same Department as 'John Doe'.sql
+
+
 SELECT * FROM employees 
 WHERE 
 department_id = (SELECT department_id FROM employees WHERE first_name = 'John' AND last_name = 'Doe');
