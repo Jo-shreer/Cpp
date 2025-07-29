bf4557d7cfac05a3b004939edb8904d6ceade922
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-07-29 21:05:21 +0100

    Update 08.insertElementinArray.cpp

--- Problems/Problem1/08.insertElementinArray.cpp
+++ Problems/Problem1/08.insertElementinArray.cpp
@@ -11,20 +11,20 @@ int main()
     int insertIndex = 3;
 
     for (int i = 0; i < insertIndex; ++i) 
-	{
+    {
         newArr[i] = arr[i];
     }
 
     newArr[insertIndex] = valueToInsert;
 
     for (int i = insertIndex; i < originalSize; ++i) 
-	{
+    {
         newArr[i + 1] = arr[i];
     }
 
     std::cout << "Modified array: ";
     for (int i = 0; i < newSize; ++i) 
-	{
+    {
         std::cout << newArr[i] << " ";
     }
     std::cout << std::endl;
